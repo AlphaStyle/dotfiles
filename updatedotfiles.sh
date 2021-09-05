b57e81d5f79560da4b2488ad25726a164a8e1e07
AlphaStyle <torbjorn1991@hotmail.com>
2021-09-05 10:07:56 +0200

    deleted mistaken uploads

--- updatedotfiles.sh
+++ updatedotfiles.sh
@@ -37,29 +37,65 @@ function compareFiles {
     fi
 }
 
-VIM_LOCAL="$HOME/.dotfiles/nvim/init.vim"
+REPO="$HOME/Downloads/dotfiles"
+
+# VIM_LOCAL="$HOME/.dotfiles/nvim/init.vim"
+VIM_LOCAL="$REPO/nvim/init.vim"
 VIM_EXTERNAL="$HOME/.config/nvim/init.vim"
 compareFiles $VIM_LOCAL $VIM_EXTERNAL
 
-ZSH_LOCAL="$HOME/.dotfiles/.zshrc"
+# ZSH_LOCAL="$HOME/.dotfiles/.zshrc"
+ZSH_LOCAL="$REPO/.zshrc"
 ZSH_EXTERNAL="$HOME/.zshrc"
 compareFiles $ZSH_LOCAL $ZSH_EXTERNAL
 
-FISH_LOCAL="$HOME/.dotfiles/.config/fish/config.fish"
+# FISH_LOCAL="$HOME/.dotfiles/.config/fish/config.fish"
+FISH_LOCAL="$REPO/fish/config.fish"
 FISH_EXTERNAL="$HOME/.config/fish/config.fish"
 compareFiles $FISH_LOCAL $FISH_EXTERNAL
 
-TMUX_LOCAL="$HOME/.dotfiles/.tmux.conf"
+XRESOURCES_LOCAL="$REPO/.Xresources"
+XRESOURCES_EXTERNAL="$HOME/.Xresources"
+compareFiles $XRESOURCES_LOCAL $XRESOURCES_EXTERNAL
+
+# TMUX_LOCAL="$HOME/.dotfiles/.tmux.conf"
+TMUX_LOCAL="$REPO/.tmux.conf"
 TMUX_EXTERNAL="$HOME/.tmux.conf"
 compareFiles $TMUX_LOCAL $TMUX_EXTERNAL
 
-TERMITE_LOCAL="$HOME/.dotfiles/termite/config"
+# TERMITE_LOCAL="$HOME/.dotfiles/termite/config"
+TERMITE_LOCAL="$REPO/termite/config"
 TERMITE_EXTERNAL="$HOME/.config/termite/config"
 compareFiles $TERMITE_LOCAL $TERMITE_EXTERNAL
 
-ALACRITTY_LOCAL="$HOME/.dotfiles/alacritty/alacritty.yml"
+# ALACRITTY_LOCAL="$HOME/.dotfiles/alacritty/alacritty.yml"
+ALACRITTY_LOCAL="$REPO/alacritty/alacritty.yml"
 ALACRITTY_EXTERNAL="$HOME/.config/alacritty/alacritty.yml"
 compareFiles $ALACRITTY_LOCAL $ALACRITTY_EXTERNAL
+
+SWAY_LOCAL="$REPO/sway/config"
+SWAY_EXTERNAL="$HOME/.config/sway/config"
+compareFiles $SWAY_LOCAL $SWAY_EXTERNAL
+
+SWAYNAG_LOCAL="$REPO/swaynag/config"
+SWAYNAG_EXTERNAL="$HOME/.config/swaynag/config"
+compareFiles $SWAYNAG_LOCAL $SWAYNAG_EXTERNAL
+
+WAYBAR_CONFIG_LOCAL="$REPO/waybar/config"
+WAYBAR_CONFIG_EXTERNAL="$HOME/.config/wofi/config"
+compareFiles $WAYBAR_CONFIG_LOCAL $WAYBAR_CONFIG_EXTERNAL
+
+WAYBAR_STYLE_LOCAL="$REPO/waybar/style.css"
+WAYBAR_STYLE_EXTERNAL="$HOME/.config/wofi/style.css"
+compareFiles $WAYBAR_STYLE_LOCAL $WAYBAR_STYLE_EXTERNAL
+
+WOFI_LOCAL="$REPO/wofi/style.css"
+WOFI_EXTERNAL="$HOME/.config/wofi/style.css"
+compareFiles $WOFI_LOCAL $WOFI_EXTERNAL
+
+MAKO_LOCAL="$REPO/mako/config"
+MAKO_EXTERNAL="$HOME/.config/mako/config"
+compareFiles $MAKO_LOCAL $MAKO_EXTERNAL
 
 if [ run_git_push ]; then
     git commit -m "Updated: $(date +%F)"
